5ec78a2772a95a4e74895c0608abcc7ec943101e
Ro <rogelio.sosa@iteso.mx>
2026-04-27 13:43:38 -0600

    ambos logica

--- app.py
+++ app.py
@@ -10,6 +10,15 @@ def suma():
         return jsonify(resultado=a + b)
     except (TypeError, ValueError):
         return jsonify(error="Parámetros 'a' y 'b' deben ser números"), 400
-    
+
+@app.route('/resta')
+def resta():
+    try:
+        a = float(request.args.get('a'))
+        b = float(request.args.get('b'))
+        return jsonify(resultado=a - b)
+    except (TypeError, ValueError):
+        return jsonify(error="Parámetros 'a' y 'b' deben ser números"), 400
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
